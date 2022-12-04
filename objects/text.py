37cdfaabdb1a88451708f9375d363eced6a4dce7
FloatingComet62 <shubh2007rai@gmail.com>
2022-12-04 21:10:55 +0530

    Text and FPS

---
+++ objects/text.py
@@ -0,0 +1,32 @@
+from pygame import font, Surface
+from util import Position, Color
+
+
+class Text:
+    string: str
+    position: Position
+    size: int
+    color: Color
+    font: str
+
+    def __init__(
+        self,
+        string: str,
+        position: Position,
+        size: int,
+        color: Color,
+        font_name: str = "freesansbold.ttf"
+    ):
+        self.string = string
+        self.position = position
+        self.size = size
+        self.color = color
+        self.font = font_name
+
+    def display(self, screen: Surface):
+        font_render = font.Font(self.font, self.size)
+        text = font_render.render(self.string, True, self.color.toRGB())
+        rect = text.get_rect()
+        rect.center = self.position.to_tuple()
+
+        screen.blit(text, rect)
--- window.py
+++ window.py
@@ -8,6 +8,7 @@ from objects.ellipse import Ellipse
 from objects.line import Line
 from objects.polygon import Polygon
 from objects.rectangle import Rectangle
+from objects.text import Text
 
 from util import Dimension, Color
 
@@ -17,19 +18,25 @@ class Window:
     dimensions: Dimension
     background_color: Color
     screen: pygame.Surface
+    clock: pygame.time
+    fps: int
     running: bool
     mouse: tuple[int, int]
-    objs: list[Union[AALine, Arc, Circle, Ellipse, Line, Polygon, Rectangle]]
+    objs: list[Union[AALine, Arc, Circle, Ellipse, Line, Polygon, Rectangle, Text]]
 
-    def __init__(self, name: str, dimensions: Dimension, background_color: Color):
+    def __init__(self, name: str, dimensions: Dimension, background_color: Color, fps: int):
         self.name = name
         self.dimensions = dimensions
         self.background_color = background_color
         self.screen = pygame.display.set_mode([dimensions.width, dimensions.height])
+        self.clock = pygame.time.Clock()
+        self.fps = fps
         self.running = True
         self.mouse = (0, 0)
         self.objs = []
         self.keys = []
+
+        pygame.init()
 
     def display(self, main):
         while self.running:
@@ -40,17 +47,19 @@ class Window:
             self.screen.fill(self.background_color.toRGB())
             self.mouse = pygame.mouse.get_pos()
             self.keys = pygame.key.get_pressed()
-            main()
+            if not main():
+                break
 
             for obj in self.objs:
                 obj.display(self.screen)
 
+            self.clock.tick(self.fps)
             pygame.display.flip()
             pygame.display.set_caption(self.name)
 
-    def addObj(self, obj: Union[AALine, Arc, Circle, Ellipse, Line, Polygon, Rectangle]):
+    def addObj(self, obj: Union[AALine, Arc, Circle, Ellipse, Line, Polygon, Rectangle, Text]):
         self.objs.append(obj)
 
-    def addObjs(self, objs: list[Union[AALine, Arc, Circle, Ellipse, Line, Polygon, Rectangle]]):
+    def addObjs(self, objs: list[Union[AALine, Arc, Circle, Ellipse, Line, Polygon, Rectangle, Text]]):
         for obj in objs:
             self.objs.append(obj)
